c30eee69eb1f5dd0c8fd616da62b930a34fccede
zeroBit <luisivan001@knights.ucf.edu>
2019-03-28 03:23:09 -0400

    updating the mal-network file

--- listing-4-8.py
+++ listing-4-8.py
@@ -38,7 +38,7 @@ def find_hostnames(string):
 
 def create_node(name, label):
     with driver.session() as session:
-        session.run("CREATE (n: $name {label: $label})", name=name, label=label)
+        session.run("CREATE (n:{} {label: $label})".format(name), label=label)
 
 def create_edge(node1,node2):
     with driver.session() as session:
